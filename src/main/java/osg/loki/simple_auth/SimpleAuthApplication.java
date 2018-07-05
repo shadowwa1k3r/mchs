2cb7d8e24b9b1bcab6ecbc2a1a811c459bbbe398
ergash1994@gmail.com <nightfire218>
2018-07-05 18:17:15 +0500

    second commit

--- src/main/java/osg/loki/simple_auth/SimpleAuthApplication.java
+++ src/main/java/osg/loki/simple_auth/SimpleAuthApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleAuthApplication  extends SpringBootServletInitializer{
+public class SimpleAuthApplication /* extends SpringBootServletInitializer*/{
 
 	
-	@Override
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 	
 		return builder.sources(SimpleAuthApplication.class);
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleAuthApplication.class, args);
